6c801f104b1afa0ea5eeb5ddc53055bf6afe3824
Alameer Ashraf <alameerelnagar94@gmail.com>
2016-09-29 02:14:26 +0200

    Add project files.

---
+++ TheFirstKick/Models/ServiceItemConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+
+
+namespace TheFirstKick.Models
+{
+    public class ServiceItemConfiguration : EntityTypeConfiguration<ServiceItem>
+    {
+        public ServiceItemConfiguration()
+        {
+            Property(s => s.ServiceItemCode)
+                .HasMaxLength(15)
+                .IsRequired()
+                .HasColumnAnnotation("Index"
+               , new IndexAnnotation(new IndexAttribute("AK_ServiceItem_ServiceItemCode") { IsUnique = true }));
+
+            Property(s => s.ServiceItemName)
+                .HasMaxLength(18)
+                .IsRequired()
+                .HasColumnAnnotation("Index",
+                new IndexAnnotation(new IndexAttribute("AK_ServiceItem_ServiceItemName") { IsUnique = true }));
+        }
+    }
+}
---
+++ TheFirstKick/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheFirstKick.Startup))]
+namespace TheFirstKick
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
