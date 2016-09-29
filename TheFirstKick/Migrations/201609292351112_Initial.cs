28e9abdb19966ff2966b96153d6a8bb6398e41eb
Alameer Ashraf <alameerelnagar94@gmail.com>
2016-09-30 01:58:08 +0200

    Entity Framework Configuration !

---
+++ TheFirstKick/Migrations/201609292351112_Initial.cs
@@ -0,0 +1,229 @@
+namespace TheFirstKick.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Categoryid = c.Int(nullable: false, identity: true),
+                        CategoryName = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Categoryid)
+                .Index(t => t.CategoryName, unique: true, name: "AK_Category_CategoryName");
+            
+            CreateTable(
+                "dbo.InventoryItems",
+                c => new
+                    {
+                        InventoryItemid = c.Int(nullable: false, identity: true),
+                        InventoryItemCode = c.String(nullable: false, maxLength: 15),
+                        InventoryItemName = c.String(nullable: false, maxLength: 80),
+                        UnitPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Categoryid = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.InventoryItemid)
+                .ForeignKey("dbo.Categories", t => t.Categoryid, cascadeDelete: true)
+                .Index(t => t.InventoryItemCode, unique: true, name: "AK_InventoryItem_InventoryItemCode")
+                .Index(t => t.InventoryItemName, unique: true, name: "AK_InventoryItem_InventoryItemName")
+                .Index(t => t.Categoryid);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerId = c.Int(nullable: false, identity: true),
+                        AccountantNumber = c.String(nullable: false, maxLength: 8),
+                        CompanyName = c.String(nullable: false, maxLength: 30),
+                        Address = c.String(nullable: false, maxLength: 30),
+                        City = c.String(nullable: false, maxLength: 15),
+                        State = c.String(nullable: false, maxLength: 2),
+                        ZipCode = c.String(nullable: false, maxLength: 10),
+                        Phone = c.String(maxLength: 15),
+                    })
+                .PrimaryKey(t => t.CustomerId)
+                .Index(t => t.AccountantNumber, unique: true, name: "AK_Customer_AccountantNumber");
+            
+            CreateTable(
+                "dbo.Labors",
+                c => new
+                    {
+                        LaborId = c.Int(nullable: false, identity: true),
+                        WorkOrderId = c.Int(nullable: false),
+                        ServiceItemCode = c.String(nullable: false, maxLength: 15),
+                        ServiceItemName = c.String(nullable: false, maxLength: 80),
+                        LaborHours = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Rate = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ExtendedPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Notes = c.String(maxLength: 140),
+                    })
+                .PrimaryKey(t => t.LaborId)
+                .ForeignKey("dbo.WorkOrders", t => t.WorkOrderId, cascadeDelete: true)
+                .Index(t => new { t.WorkOrderId, t.ServiceItemCode }, unique: true, name: "AK_labor");
+            
+            CreateTable(
+                "dbo.WorkOrders",
+                c => new
+                    {
+                        WorkOrderId = c.Int(nullable: false, identity: true),
+                        CustomerId = c.Int(nullable: false),
+                        OrderDateTime = c.DateTime(nullable: false , defaultValueSql:"GetDate()"),
+                        TargetDateTime = c.DateTime(),
+                        DrfopDeadDateTime = c.DateTime(),
+                        Description = c.String(maxLength: 256),
+                        WorkOrderStatus = c.Int(nullable: false),
+                        Total = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CertificationRequierments = c.String(maxLength: 120),
+                        ReworkNotes = c.String(),
+                    })
+                .PrimaryKey(t => t.WorkOrderId)
+                .ForeignKey("dbo.Customers", t => t.CustomerId, cascadeDelete: true)
+                .Index(t => t.CustomerId);
+            
+            CreateTable(
+                "dbo.Parts",
+                c => new
+                    {
+                        Partid = c.Int(nullable: false, identity: true),
+                        WorkOrderId = c.Int(nullable: false),
+                        InventoryItemCode = c.String(nullable: false, maxLength: 15),
+                        InventoryItemName = c.String(nullable: false, maxLength: 80),
+                        Quentity = c.Int(nullable: false),
+                        UnitPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ExtendedPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Notes = c.String(maxLength: 140),
+                        IsInstalled = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Partid)
+                .ForeignKey("dbo.WorkOrders", t => t.WorkOrderId, cascadeDelete: true)
+                .Index(t => new { t.WorkOrderId, t.InventoryItemCode }, unique: true, name: "AK_Part");
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.ServiceItems",
+                c => new
+                    {
+                        ServiceItemId = c.Int(nullable: false, identity: true),
+                        ServiceItemCode = c.String(nullable: false, maxLength: 15),
+                        ServiceItemName = c.String(nullable: false, maxLength: 80),
+                        Rate = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ServiceItemId)
+                .Index(t => t.ServiceItemCode, unique: true, name: "AK_ServiceItem_ServiceItemCode")
+                .Index(t => t.ServiceItemName, unique: true, name: "AK_ServiceItem_ServiceItemName");
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Parts", "WorkOrderId", "dbo.WorkOrders");
+            DropForeignKey("dbo.Labors", "WorkOrderId", "dbo.WorkOrders");
+            DropForeignKey("dbo.WorkOrders", "CustomerId", "dbo.Customers");
+            DropForeignKey("dbo.InventoryItems", "Categoryid", "dbo.Categories");
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.ServiceItems", "AK_ServiceItem_ServiceItemName");
+            DropIndex("dbo.ServiceItems", "AK_ServiceItem_ServiceItemCode");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Parts", "AK_Part");
+            DropIndex("dbo.WorkOrders", new[] { "CustomerId" });
+            DropIndex("dbo.Labors", "AK_labor");
+            DropIndex("dbo.Customers", "AK_Customer_AccountantNumber");
+            DropIndex("dbo.InventoryItems", new[] { "Categoryid" });
+            DropIndex("dbo.InventoryItems", "AK_InventoryItem_InventoryItemName");
+            DropIndex("dbo.InventoryItems", "AK_InventoryItem_InventoryItemCode");
+            DropIndex("dbo.Categories", "AK_Category_CategoryName");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.ServiceItems");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Parts");
+            DropTable("dbo.WorkOrders");
+            DropTable("dbo.Labors");
+            DropTable("dbo.Customers");
+            DropTable("dbo.InventoryItems");
+            DropTable("dbo.Categories");
+        }
+    }
+}
--- TheFirstKick/Models/IdentityModels.cs
+++ TheFirstKick/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using TheFirstKick.DataLayer;
 
 namespace TheFirstKick.Models
 {
@@ -20,9 +21,33 @@ namespace TheFirstKick.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        //this where we will do all the WORK RELATED TO CONTEXT !
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<Labor> Labors { get; set; }
+        public DbSet<ServiceItem> ServiceItems { get; set; }
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+
+
+        // NOW TO TELL EF WHERE TO FIND THE CONFIGURATION WE NEED TO CREATE OUR DATABASE 
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CategoryConfiguration());
+            modelBuilder.Configurations.Add(new CustomerConfiguration());
+            modelBuilder.Configurations.Add(new InventoryItemConfiguration());
+            modelBuilder.Configurations.Add(new PartConfiguration());
+            modelBuilder.Configurations.Add(new LaborConfiguration());
+            modelBuilder.Configurations.Add(new ServiceItemConfiguration());
+            modelBuilder.Configurations.Add(new WorkOrderConfiguration());
+            base.OnModelCreating(modelBuilder);
         }
 
         public static ApplicationDbContext Create()
@@ -31,3 +56,12 @@ namespace TheFirstKick.Models
         }
     }
 }
+
+
+//enable-migrations -contexttypename ApplicationDbContext
+/*
+ *   When you develop a new application, your data model changes frequently, and each time the model changes,
+ *  it gets out of sync with the database. You have configured the Entity Framework to automatically drop and re-create the database each time you change the data model. When you add, remove, or change entity classes or change your DbContext class, the next time you run the application it automatically deletes your existing database,
+ *  creates a new one that matches the model, and seeds it with test data.
+ */
+ //add-migration Initial 
